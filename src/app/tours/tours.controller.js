016ac8b9e8b9a1baeacc2419345db3b6039bf944
Ivan Kuznetsov <me@jeiwan.ru>
2015-10-13 19:11:01 +0300

    Hide form after adding a new tour

--- src/app/tours/tours.controller.js
+++ src/app/tours/tours.controller.js
@@ -20,22 +20,28 @@ export function ToursController() {
 ToursController.prototype.addTour = function() {
   this.tours.push(this.newTour);
   this.newTour = new Tour();
+  this.hideForm();
+
+  return true;
 }
 
 ToursController.prototype.removeTour = function(tour) {
   this.tours = this.tours.filter((e) => {
     return e.$$hashKey !== tour.$$hashKey;
   });
+
   return true;
 }
 
 ToursController.prototype.showForm = function() {
   this.formVisible = true;
+
   return true;
 }
 
 ToursController.prototype.hideForm = function() {
   this.formVisible = false;
+
   return true;
 }
 
