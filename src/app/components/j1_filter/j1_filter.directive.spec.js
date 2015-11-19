a5288cffca22b3b202163ca1834d6e2b84eb8673
Ivan Kuznetsov <me@jeiwan.ru>
2015-11-19 19:16:46 +0700

    Lesson 8

--- src/app/components/j1_filter/j1_filter.directive.spec.js
+++ src/app/components/j1_filter/j1_filter.directive.spec.js
@@ -1,5 +1,5 @@
 describe('j1Filter', () => {
-  var $compile, scope, j1Filters = [], iScopes = [];
+  var $compile, scope, highPriorityFilter, lowPriorityFilter;
 
   beforeEach(() => {
     window.module('TravellerNG');
@@ -19,47 +19,47 @@ describe('j1Filter', () => {
       { name: 'AAA', relation: { name: 'XXX' } }
     ];
 
-    var html1 = "<j1-filter items='items' field='name' priority='1'>Clear</j1-filter>",
-        html2 = "<j1-filter items='items' field='relation.name' priority='2'>Clear</j1-filter>";
+    var highPriorityFilterHtml = "<j1-filter items='items' field='name' priority='1'>Clear</j1-filter>",
+        lowPriorityFilterHtml = "<j1-filter items='items' field='relation.name' priority='2'>Clear</j1-filter>",
+        highPriorityFilterElement = $compile(angular.element(highPriorityFilterHtml))(scope),
+        lowPriorityFilterElement = $compile(angular.element(lowPriorityFilterHtml))(scope);
 
-    j1Filters[0] = $compile(angular.element(html1))(scope);
-    j1Filters[1] = $compile(angular.element(html2))(scope);
     scope.$digest();
-    iScopes[0] = j1Filters[0].isolateScope();
-    iScopes[1] = j1Filters[1].isolateScope();
+    highPriorityFilter = { element: highPriorityFilterElement, scope: highPriorityFilterElement.isolateScope() };
+    lowPriorityFilter = { element: lowPriorityFilterElement, scope: lowPriorityFilterElement.isolateScope() };
   });
 
   describe('linking', () => {
     it('applies template', () => {
-      expect(j1Filters[0].html()).not.toEqual('');
-      expect(j1Filters[0].find('.j1Filters')).toBeDefined();
-      expect(j1Filters[1].html()).not.toEqual('');
-      expect(j1Filters[1].find('.j1Filters')).toBeDefined();
+      expect(highPriorityFilter.element.html()).not.toEqual('');
+      expect(highPriorityFilter.element.find('.j1Filters')).toBeDefined();
+      expect(lowPriorityFilter.element.html()).not.toEqual('');
+      expect(lowPriorityFilter.element.find('.j1Filters')).toBeDefined();
     });
 
     it('sets items', () => {
-      expect(iScopes[0].items).toEqual(scope.items);
-      expect(iScopes[1].items).toEqual(scope.items);
+      expect(highPriorityFilter.scope.items).toEqual(scope.items);
+      expect(highPriorityFilter.scope.items).toEqual(scope.items);
     });
 
     it('sets field', () => {
-      expect(iScopes[0].field).toEqual('name');
-      expect(iScopes[1].field).toEqual('relation.name');
+      expect(highPriorityFilter.scope.field).toEqual('name');
+      expect(lowPriorityFilter.scope.field).toEqual('relation.name');
     });
 
     it('sets priproty', () => {
-      expect(iScopes[0].priority).toEqual('1');
-      expect(iScopes[1].priority).toEqual('2');
+      expect(highPriorityFilter.scope.priority).toEqual('1');
+      expect(lowPriorityFilter.scope.priority).toEqual('2');
     });
 
     it('sets selectedItem', () => {
-      expect(iScopes[0].selectedItem).toEqual(null);
-      expect(iScopes[1].selectedItem).toEqual(null);
+      expect(highPriorityFilter.scope.selectedItem).toEqual(null);
+      expect(lowPriorityFilter.scope.selectedItem).toEqual(null);
     });
 
     it('builds values list', () => {
-      expect(iScopes[0].values).toEqual(['AAA', 'BBB', 'CCC']);
-      expect(iScopes[1].values).toEqual(['XXX', 'YYY', 'ZZZ']);
+      expect(highPriorityFilter.scope.values).toEqual(['AAA', 'BBB', 'CCC']);
+      expect(lowPriorityFilter.scope.values).toEqual(['XXX', 'YYY', 'ZZZ']);
     });
   });
 
@@ -67,33 +67,25 @@ describe('j1Filter', () => {
     describe('first filter', () => {
       describe('when filter is applied', () => {
         it('correctly sets "filtered" property of item', () => {
-          iScopes[0].selectedItem = iScopes[0].values[1];
-          iScopes[0].$digest();
-          expect(iScopes[0].items.filter(i => i.filtered).map(i => i.name)).toEqual(['BBB', 'BBB']);
-
-          iScopes[0].selectedItem = iScopes[0].values[2];
-          iScopes[0].$digest();
-          expect(iScopes[0].items.filter(i => i.filtered).map(i => i.name)).toEqual(['CCC']);
-
-          iScopes[0].selectedItem = iScopes[0].values[0];
-          iScopes[0].$digest();
-          expect(iScopes[0].items.filter(i => i.filtered).map(i => i.name)).toEqual(['AAA', 'AAA', 'AAA', 'AAA']);
+          highPriorityFilter.scope.selectedItem = highPriorityFilter.scope.values[2];
+          highPriorityFilter.scope.$digest();
+          expect(highPriorityFilter.scope.items.filter(i => i.filtered).map(i => i.name)).toEqual(['CCC']);
         });
 
         it('rebuilds second filter values', () => {
-          iScopes[0].selectedItem = iScopes[0].values[1];
-          iScopes[0].$digest();
-          expect(iScopes[1].values).toEqual(['YYY', 'ZZZ']);
+          highPriorityFilter.scope.selectedItem = highPriorityFilter.scope.values[1];
+          highPriorityFilter.scope.$digest();
+          expect(lowPriorityFilter.scope.values).toEqual(['YYY', 'ZZZ']);
         });
       });
 
       describe('when filter is cleared', () => {
         it('shows all items', () => {
-          iScopes[0].selectedItem = iScopes[0].values[1];
-          iScopes[0].$digest();
-          iScopes[0].resetFilter();
-          iScopes[0].$digest();
-          expect(iScopes[0].items.filter(i => i.filtered).map(i => i.name)).toEqual(iScopes[0].items.map(i => i.name));
+          highPriorityFilter.scope.selectedItem = highPriorityFilter.scope.values[1];
+          highPriorityFilter.scope.$digest();
+          highPriorityFilter.scope.resetFilter();
+          highPriorityFilter.scope.$digest();
+          expect(highPriorityFilter.scope.items.filter(i => i.filtered).map(i => i.name)).toEqual(highPriorityFilter.scope.items.map(i => i.name));
         });
       });
     });
@@ -101,33 +93,25 @@ describe('j1Filter', () => {
     describe('second filter', () => {
       describe('when filter is applied', () => {
         it('correctly sets "filtered" property of item', () => {
-          iScopes[1].selectedItem = iScopes[1].values[1];
-          iScopes[1].$digest();
-          expect(iScopes[1].items.filter(i => i.filtered).map(i => i.relation.name)).toEqual(['YYY']);
-
-          iScopes[1].selectedItem = iScopes[1].values[2];
-          iScopes[1].$digest();
-          expect(iScopes[1].items.filter(i => i.filtered).map(i => i.relation.name)).toEqual(['ZZZ', 'ZZZ']);
-
-          iScopes[1].selectedItem = iScopes[1].values[0];
-          iScopes[1].$digest();
-          expect(iScopes[1].items.filter(i => i.filtered).map(i => i.relation.name)).toEqual(['XXX', 'XXX', 'XXX', 'XXX']);
+          lowPriorityFilter.scope.selectedItem = lowPriorityFilter.scope.values[2];
+          lowPriorityFilter.scope.$digest();
+          expect(lowPriorityFilter.scope.items.filter(i => i.filtered).map(i => i.relation.name)).toEqual(['ZZZ', 'ZZZ']);
         });
 
         it('does not rebuild first filter values', () => {
-          iScopes[1].selectedItem = iScopes[1].values[1];
-          iScopes[1].$digest();
-          expect(iScopes[0].values).toEqual(['AAA', 'BBB', 'CCC']);
+          highPriorityFilter.scope.selectedItem = highPriorityFilter.scope.values[1];
+          highPriorityFilter.scope.$digest();
+          expect(highPriorityFilter.scope.values).toEqual(['AAA', 'BBB', 'CCC']);
         });
       });
 
       describe('when filter is cleared', () => {
         it('shows all items', () => {
-          iScopes[1].selectedItem = iScopes[1].values[1];
-          iScopes[1].$digest();
-          iScopes[1].resetFilter();
-          iScopes[1].$digest();
-          expect(iScopes[1].items.filter(i => i.filtered).map(i => i.relation.name)).toEqual(iScopes[1].items.map(i => i.relation.name));
+          lowPriorityFilter.scope.selectedItem = lowPriorityFilter.scope.values[1];
+          lowPriorityFilter.scope.$digest();
+          lowPriorityFilter.scope.resetFilter();
+          lowPriorityFilter.scope.$digest();
+          expect(lowPriorityFilter.scope.items.filter(i => i.filtered).map(i => i.relation.name)).toEqual(lowPriorityFilter.scope.items.map(i => i.relation.name));
         });
       });
     });
